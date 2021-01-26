a3f7edb3f9ae37d0136fe363906c8011ae9c0724
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-01-26 17:39:18 -0300

    add styled wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import PlayButton from '../src/components/PlayButton';
+import PlayInput from '../src/components/PlayInput';
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+  color: ${({ theme }) => theme.colors.contrastText};
+`;
 
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `
+const Subtitle = styled.h2`
+  font-size: 40px;
+  color: ${({ theme }) => theme.colors.contrastText};
+`;
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -26,31 +28,63 @@ const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+`;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
           <Widget.Header>
-            <h1>The legend of Node.js</h1>
+            <Title>Mundo do JavaScript</Title>
           </Widget.Header>
           <Widget.Content>
-            <p>Node é um bagulho muito loco de bom.</p>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Wrapper>
+                <Subtitle>
+                  Teste os seus conhecimentos sobre o universo do JavaScript e
+                  divirta-se criando o seu AluraQuiz!
+                </Subtitle>
+                <PlayInput
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Diz aí seu nome para jogar :)"
+                  maxLength={30}
+                  type="text"
+                />
+                <PlayButton type="submit" disabled={name.length === 0}>
+                  JOGAR
+                </PlayButton>
+              </Wrapper>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Header>
-            <h1>The legend of Node.js</h1>
+            <Title>World of JavaScript</Title>
           </Widget.Header>
           <Widget.Content>
-            <p>Node é um bagulho muito loco de bom.</p>
+            <p>JavaScript é um bagulho muito loco de bom.</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/mrbrunelli" />
     </QuizBackground>
-  )
+  );
 }
