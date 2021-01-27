b3c613bdf9b1eb9c560af7191dbfd841b77a2db5
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-01-27 18:48:54 -0300

    move Wrapper to a separated folder

---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+`;
+
+export default Wrapper;
